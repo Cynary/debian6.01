7fdff31d40e6ff1eeb4be7753b092c0a10718bf1
Cynay <rodrigo.toste.gomes@gmail.com>
2016-02-17 11:36:26 -0500

    Make installer actually preserve file permissions and add them to unexisting files

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
-apt-get install firmware-iwlwifi emacs-nox dhcpcd5 git build-essential tmux libpam-krb5 -y
-apt-get remove wicd -y
-apt-get autoremove -y
+#apt-get install firmware-iwlwifi emacs-nox dhcpcd5 git build-essential tmux libpam-krb5 -y
+#apt-get remove wicd -y
+#apt-get autoremove -y
 
-systemctl enable dhcpcd
-echo "alias python=python3" >> /etc/bash.bashrc
+#systemctl enable dhcpcd
+#echo "alias python=python3" >> /etc/bash.bashrc
 
-cp -R --no-preserve=all slash/* /
+# Special added files+directory
+cp -r slash/etc/pam.d/pam_exec.d /etc/pam.d
+chmod 755 /etc/pam.d/pam_exec.d/*
+chown -R 0:0 /etc/pam.d/pam_exec.d
+
+for f in `find slash -type f | cut -d'/' -f2-`
+do
+    PERMS=`stat -c "%a" /$f`
+    OWNER=`stat -c "%u" /$f`
+    GROUP=`stat -c "%g" /$f`
+    echo $f $PERMS $OWNER $GROUP
+    cp -R --no-preserve=all slash/$f /$f
+    chmod $PERMS /$f
+    chown $OWNER:$GROUP /$f
+done
