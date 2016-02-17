fcac60a40f14a052e43bc3b5da1c5b8f3301b0c5
Cynay <rodrigo.toste.gomes@gmail.com>
2016-02-17 11:52:57 -0500

    Adding firmware as downloadable file; avoids install of unnecessary firmware and makes it so we can avoid using non-free sources

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
-#apt-get install firmware-iwlwifi emacs-nox dhcpcd5 git build-essential tmux libpam-krb5 -y
-#apt-get remove wicd -y
-#apt-get autoremove -y
+apt-get install emacs-nox dhcpcd5 git build-essential tmux libpam-krb5 -y
+apt-get remove wicd -y
+apt-get autoremove -y
 
-#systemctl enable dhcpcd
-#echo "alias python=python3" >> /etc/bash.bashrc
+systemctl enable dhcpcd
+echo "alias python=python3" >> /etc/bash.bashrc
 
 # Special added files+directory
 cp -r slash/etc/pam.d/pam_exec.d /etc/pam.d
 chmod 755 /etc/pam.d/pam_exec.d/*
 chown -R 0:0 /etc/pam.d/pam_exec.d
 
+cp slash/lib/firmware/iwlwifi-1000-5.ucode /lib/firmware
+chmod 755 /lib/firmware/iwlwifi-1000-5.ucode
+chown -R 0:0 /lib/firmware/iwlwifi-1000-5.ucode
+
 for f in `find slash -type f | cut -d'/' -f2-`
 do
     PERMS=`stat -c "%a" /$f`
     OWNER=`stat -c "%u" /$f`
     GROUP=`stat -c "%g" /$f`
-    echo $f $PERMS $OWNER $GROUP
     cp -R --no-preserve=all slash/$f /$f
     chmod $PERMS /$f
     chown $OWNER:$GROUP /$f
