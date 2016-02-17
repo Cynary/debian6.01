557cd26264253d6b8aa7a4a64291f291f222d44a
Cynay <rodrigo.toste.gomes@gmail.com>
2016-02-17 13:10:52 -0500

    Made install_network better

--- install_network.sh
+++ install_network.sh
@@ -1,35 +1,12 @@
 #!/usr/bin/env bash
-apt-get install emacs-nox dhcpcd5 git build-essential tmux libpam-krb5 -y
-apt-get remove wicd -y
-apt-get autoremove -y
+apt-get install git -y
 
-systemctl enable dhcpcd
-echo "alias python=python3" >> /etc/bash.bashrc
-
+# Get files, and execute actual installer
 # Get the files
 REPO=`mktemp -d`
 git clone http://github.com/Cynary/debian6.01 $REPO
 cd $REPO
-
-# Special added files+directory
-cp -r slash/etc/pam.d/pam_exec.d /etc/pam.d
-chmod 755 /etc/pam.d/pam_exec.d/*
-chown -R 0:0 /etc/pam.d/pam_exec.d
-
-cp slash/lib/firmware/iwlwifi-1000-5.ucode /lib/firmware
-chmod 755 /lib/firmware/iwlwifi-1000-5.ucode
-chown -R 0:0 /lib/firmware/iwlwifi-1000-5.ucode
-
-for f in `find slash -type f | cut -d'/' -f2-`
-do
-    PERMS=`stat -c "%a" /$f`
-    OWNER=`stat -c "%u" /$f`
-    GROUP=`stat -c "%g" /$f`
-    cp -R --no-preserve=all slash/$f /$f
-    chmod $PERMS /$f
-    chown $OWNER:$GROUP /$f
-done
-
-# Back to where we came from, and cleanup
+chmod +x install.sh
+./install.sh
 cd -
 rm -fr $REPO
